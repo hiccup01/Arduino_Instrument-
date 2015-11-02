a5729e5fc9e819db448ea0a87899b60521036b28
hiccup01 <yectoplasma@icloud.com>
2015-11-03 08:29:50 +1300

    bler

--- play_notes/play_notes.ino
+++ play_notes/play_notes.ino
@@ -1,7 +1,7 @@
 int incomingByte = 0;   // for incoming serial data
 
 void setup() {
-        Serial.begin(9600); // opens serial port, sets data rate to 9600 bps 
+        Serial.begin(19200); // opens serial port, sets data rate to 9600 bps 
         int tonePin = 9;
         
 }
@@ -12,8 +12,6 @@ void loop() {
         if (Serial.available() > 0) {
                 // read the incoming byte:
                 incomingByte = Serial.read();
-                incomingByte - 96;
-                incomingByte * 50000;
                 // say what you got:
                 Serial.print("I received: ");
                 Serial.println(incomingByte);
