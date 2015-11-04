53c0745f2458e023498c89475e4d72e4af9e273a
hiccup01 <yectoplasma@icloud.com>
2015-11-04 18:47:55 +1300

    Fixed code

--- resistance_notes.ino/resistance_notes.ino.ino
+++ resistance_notes.ino/resistance_notes.ino.ino
@@ -1,14 +1,26 @@
+int resistance = 0;
+int test = 0;
+int test2 = 0;
+const int tonePin = 9;
+const int resistancePin = 7;
 void setup() {
-        int tonePin = 9;
-        pinMode(8, OUTPUT); // Set pin 8 to output current
-        digitalWrite(8, HIGH);
-        pinMode(7, INPUT); // Set pin 7 to be an input
-        Serial.begin(9600);
-        int resistance = 0; // declear vars
+         // declear vars
         
+        const int powerPin = 8;
+        pinMode(powerPin, OUTPUT); // Set pin 8 to output current
+        digitalWrite(powerPin, HIGH);
+        pinMode(resistancePin, INPUT); // Set pin 7 to be an input
+        Serial.begin(9600);
 }
 
 void loop() {
-  
-        
+  test2 = 0;
+  while(test2 < 40); {
+  resistance = digitalRead(resistancePin);
+  test = test + resistance;
+  test2 = test2 + 1;
+  }
+  if (test = 40) {
+    tone(tonePin, 50);
+  }
 }
